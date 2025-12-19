a75631c14d63a1ab30f95c82fb26ae677cdc524c
Яна <pomalejkoana@gmail.com>
2025-12-19 20:45:35 +0500

    dsd

--- tasks/task1.py
+++ tasks/task1.py
@@ -4,6 +4,19 @@
 
 def solve():
 # # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+    try:
+    num1_input = input("Введите первое число: ")
+    num2_input = input("Введите второе число: ")
+
+    # Преобразуем введенные строки в целые числа
+    num1 = int(num1_input)
+    num2 = int(num2_input)
+
+    total_sum = num1 + num2
+
+    print(f"Сумма: {total_sum}")
+    Ыexcept ValueError:
+    print("Ошибка: Пожалуйста, введите целые числа.")
 
 
 
